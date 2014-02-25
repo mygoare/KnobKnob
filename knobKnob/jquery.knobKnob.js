7e673b4c4c32e0b2d567c604ef01ad0d6536d699
mygoare <mygoare@gmail.com>
2014-02-25 17:58:35 +0800

    rewrite jquery.knobknob.js & add rotate method to modify rotate value

--- knobKnob/jquery.knobKnob.js
+++ knobKnob/jquery.knobKnob.js
@@ -7,130 +7,158 @@
  */
 
 (function($){
-	
-	$.fn.knobKnob = function(props, settings){
+
+    function KnobKnob(props, settings)
+    {
+        this.knob = null;
+        this.knobTop = null;
+        this.startDeg = -1;
+        this.currentDeg = 0;
+        this.rotation = 0;
+        this.lastDeg = 0;
+        this.doc = $(document);
 
         if (typeof props == 'object')
         {
             settings = props;
         }
-        else if (typeof props == 'string')
+
+        this.options = $.extend({}, $.fn.knobKnob.defaultSettings, settings || {});
+    }
+
+    KnobKnob.prototype =
+    {
+        generate: function(el)
         {
+            var tpl = '<div class="knob">\
+                           <div class="top"></div>\
+                           <div class="base"></div>\
+                       </div>';
 
+
+            el.append(tpl);
+
+            this.knob = $('.knob', el);
+            this.knobTop = this.knob.find('.top');
+            this.knob.addClass(this.options.className);
+
+            this.rotate(this.options.value);
+        },
+        rotate: function(v)
+        {
+            if(v > 0 && v <= 359){
+                this.rotation = this.lastDeg = this.currentDeg = v;
+                this.knobTop.css('transform','rotate('+(v)+'deg)');
+                this.options.turn(v/359);
+            }
+        },
+        bind: function()
+        {
+            var _this = this;
+            this.knob.on('mousedown touchstart', function(e)
+            {
+                e.preventDefault();
+
+                var offset = _this.knob.offset();
+                var center = {
+                    y : offset.top + _this.knob.height()/2,
+                    x: offset.left + _this.knob.width()/2
+                };
+
+                var a, b, deg, tmp,
+                    rad2deg = 180/Math.PI;
+
+                _this.knob.on('mousemove.rem touchmove.rem',function(e)
+                {
+
+                    e = (e.originalEvent.touches) ? e.originalEvent.touches[0] : e;
+
+                    a = center.y - e.pageY;
+                    b = center.x - e.pageX;
+                    deg = Math.atan2(a,b)*rad2deg;
+
+                    // we have to make sure that negative
+                    // angles are turned into positive:
+                    if(deg<0){
+                        deg = 360 + deg;
+                    }
+
+                    // Save the starting position of the drag
+                    if(_this.startDeg == -1){
+                        _this.startDeg = deg;
+                    }
+
+                    // Calculating the current rotation
+                    tmp = Math.floor((deg-_this.startDeg) + _this.rotation);
+
+                    // Making sure the current rotation
+                    // stays between 0 and 359
+                    if(tmp < 0){
+                        tmp = 360 + tmp;
+                    }
+                    else if(tmp > 359){
+                        tmp = tmp % 360;
+                    }
+
+                    // Snapping in the off position:
+                    if(_this.options.snap && tmp < _this.options.snap){
+                        tmp = 0;
+                    }
+
+                    // This would suggest we are at an end position;
+                    // we need to block further rotation.
+                    if(Math.abs(tmp - _this.lastDeg) > 180){
+                        return false;
+                    }
+
+                    _this.currentDeg = tmp;
+                    _this.lastDeg = tmp;
+
+                    _this.knobTop.css('transform','rotate('+(_this.currentDeg)+'deg)');
+                    _this.options.turn(_this.currentDeg/360);
+                });
+
+                _this.doc.on('mouseup.rem  touchend.rem',function()
+                {
+                    _this.knob.off('.rem');
+                    _this.doc.off('.rem');
+
+                    // Saving the current rotation
+                    _this.rotation = _this.currentDeg;
+
+                    // Marking the starting degree as invalid
+                    _this.startDeg = -1;
+                });
+
+            });
         }
+    };
 	
-		var options = $.extend({}, $.fn.knobKnob.defaultSettings, settings || {});
+	$.fn.knobKnob = function(props, settings)
+    {
+		return this.each(function()
+        {
+            var knobKnob = new KnobKnob(props, settings);
 
-		var tpl = '<div class="knob">\
-				       <div class="top"></div>\
-				       <div class="base"></div>\
-			       </div>';
-	
-		return this.each(function(){
-			
-			var el = $(this);
-			el.append(tpl);
-			
-			var knob = $('.knob',el),
-				knobTop = knob.find('.top'),
-				startDeg = -1,
-				currentDeg = 0,
-				rotation = 0,
-				lastDeg = 0,
-				doc = $(document);
+            if (!this.el)
+            {
+                this.el = $(this);
+                knobKnob.generate(this.el);
+                knobKnob.bind();
 
-            knob.addClass(options.className);
-			
-			if(options.value > 0 && options.value <= 359){
-				rotation = lastDeg = currentDeg = options.value;
-				knobTop.css('transform','rotate('+(currentDeg)+'deg)');
-				options.turn(currentDeg/359);
-			}
-			
-			knob.on('mousedown touchstart', function(e){
-			
-				e.preventDefault();
-			
-				var offset = knob.offset();
-				var center = {
-					y : offset.top + knob.height()/2,
-					x: offset.left + knob.width()/2
-				};
-				
-				var a, b, deg, tmp,
-					rad2deg = 180/Math.PI;
-				
-				knob.on('mousemove.rem touchmove.rem',function(e){
-					
-					e = (e.originalEvent.touches) ? e.originalEvent.touches[0] : e;
-					
-					a = center.y - e.pageY;
-					b = center.x - e.pageX;
-					deg = Math.atan2(a,b)*rad2deg;
-					
-					// we have to make sure that negative
-					// angles are turned into positive:
-					if(deg<0){
-						deg = 360 + deg;
-					}
-					
-					// Save the starting position of the drag
-					if(startDeg == -1){
-						startDeg = deg;
-					}
-					
-					// Calculating the current rotation
-					tmp = Math.floor((deg-startDeg) + rotation);
-					
-					// Making sure the current rotation
-					// stays between 0 and 359
-					if(tmp < 0){
-						tmp = 360 + tmp;
-					}
-					else if(tmp > 359){
-						tmp = tmp % 360;
-					}
-					
-					// Snapping in the off position:
-					if(options.snap && tmp < options.snap){
-						tmp = 0;
-					}
-					
-					// This would suggest we are at an end position;
-					// we need to block further rotation.
-					if(Math.abs(tmp - lastDeg) > 180){
-						return false;
-					}
-					
-					currentDeg = tmp;
-					lastDeg = tmp;
-		
-					knobTop.css('transform','rotate('+(currentDeg)+'deg)');
-					options.turn(currentDeg/360);
-				});
-			
-				doc.on('mouseup.rem  touchend.rem',function(){
-					knob.off('.rem');
-					doc.off('.rem');
-					
-					// Saving the current rotation
-					rotation = currentDeg;
-					
-					// Marking the starting degree as invalid
-					startDeg = -1;
-				});
-			
-			});
+                this.el.data('knob', knobKnob);
+            }
 		});
 	};
 
-    $.fn.knobKnob.defaultSettings = {
+    $.fn.knobKnob.defaultSettings =
+    {
         min: 0,
         max: 0,
         className: "default",
         snap: 0,
         value: 0,
-        turn: null
+        turn: function(){}
     };
 	
 })(jQuery);
